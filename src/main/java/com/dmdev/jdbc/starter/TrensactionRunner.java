1c92dd0dcdd38810df94794ab1898f8f1301e98e
Viner108 <andrukovichl@mail.ru>
2024-02-13 13:52:48 +0300

    004 Batch запросы

--- src/main/java/com/dmdev/jdbc/starter/TrensactionRunner.java
+++ src/main/java/com/dmdev/jdbc/starter/TrensactionRunner.java
@@ -5,28 +5,35 @@ import com.dmdev.jdbc.starter.until.ConnectionManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TrensactionRunner {
     public static void main(String[] args) throws SQLException {
         long book = 2;
-        String deleteBookSql = "DELETE FROM book where id = ?";
-        String deleteAuthorSql = "DELETE from author where book = ?";
+        String deleteBookSql = "DELETE FROM book where id = "+book;
+        String deleteAuthorSql = "DELETE from author where book = "+book;
         Connection connection= null;
-        PreparedStatement deleteBookStatement= null;
-        PreparedStatement deleteAuthorStatement=null;
+//        PreparedStatement deleteBookStatement= null;
+//        PreparedStatement deleteAuthorStatement=null;
+        Statement statement =null;
         try{
             connection= ConnectionManager.open();
-            deleteBookStatement = connection.prepareStatement(deleteBookSql);
-            deleteAuthorStatement = connection.prepareStatement(deleteAuthorSql);
             connection.setAutoCommit(false);
-            deleteBookStatement.setLong(1, book);
-            deleteAuthorStatement.setLong(1, book);
 
-            deleteAuthorStatement.executeUpdate();
-            if(true){
-                throw new RuntimeException("Ooooops...");
-            }
-            deleteBookStatement.executeUpdate();
+            statement = connection.createStatement();
+            statement.addBatch(deleteAuthorSql);
+            statement.addBatch(deleteBookSql);
+
+            int[] ints = statement.executeBatch();
+//            deleteBookStatement = connection.prepareStatement(deleteBookSql);
+//            deleteAuthorStatement = connection.prepareStatement(deleteAuthorSql);
+//            deleteBookStatement.setLong(1, book);
+//            deleteAuthorStatement.setLong(1, book);
+//            int deleteAuthorResult = deleteAuthorStatement.executeUpdate();
+//            if(true){
+//                throw new RuntimeException("Ooooops...");
+//            }
+//            int deleteBookResult = deleteBookStatement.executeUpdate();
             connection.commit();
         }catch (Exception e){
             if (connection!= null){
@@ -37,12 +44,15 @@ public class TrensactionRunner {
             if (connection!= null){
                 connection.close();
             }
-            if(deleteBookStatement!= null){
-                deleteBookStatement.close();
+            if (statement!=null){
+                statement.close();
             }
-            if (deleteAuthorStatement!=null){
-                deleteAuthorStatement.close();
-            }
+//            if(deleteBookStatement!= null){
+//                deleteBookStatement.close();
+//            }
+//            if (deleteAuthorStatement!=null){
+//                deleteAuthorStatement.close();
+//            }
         }
     }
 }
